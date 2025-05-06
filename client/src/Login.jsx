604b754fb67216d52b5079efaadbc9c2febf93fe
Leonardo Souza <leonardosls@al.insper.edu.br>
2025-05-06 07:47:31 -0300

    login corrigido

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -11,7 +11,7 @@ export default function Login({ onLogin }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/login", {
+      const res = await axios.post("http://127.0.0.1:5000/login", {
         email,
         senha,
       });
